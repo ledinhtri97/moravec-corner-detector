acce503459f8856078c854ebca5adf4e93e3fdb0
ledinhtri <ledinhtri.cse.hcmut@gmail.com>
2018-11-12 21:53:55 +0700

    change localmaxima

--- moravec_corner_detector.py
+++ moravec_corner_detector.py
@@ -6,27 +6,32 @@ def neighbors(arr,x,y,n=3):
 	return arr[:n,:n]
 
 im = [[0, 0, 0, 0, 0],
-	[0, 1, 0, 0, 0],
-	[0, 1, 1, 0, 0],
 	[0, 1, 1, 1, 0],
+	[0, 1, 0, 0, 0],
+	[0, 1, 0, 0, 0],
 	[0, 0, 0, 0, 0]]
 
 im = np.asarray(im)
 
 # print(im)
 
-shifts = [[1, 0], [1, 1], [0, 1], [-1, 1]]
+shifts = [[1, 1], [1, -1], [-1, 1], [-1, -1], [1, 0], [0, 1]]
 
 point = (1, 1)
 
-window = neighbors(im, 2, 2)
 
-E = []
 
-for shift in shifts:
-	wdShift = neighbors(im, point[0] + shift[1], point[1] + shift[0])
-	subWd = np.power(wdShift - window, 2)
-	E.append(np.sum(subWd));
-		
-print(E)
+localMaxima = []
+
+for x in range(1, im.shape[0]-1):
+	for y in range(1, im.shape[1]-1):
+		window = neighbors(im, x, y)
+		E = []
+		for shift in shifts:
+			wdShift = neighbors(im, point[0] + shift[1], point[1] + shift[0])
+			subWd = np.power(wdShift - window, 2)
+			E.append(np.sum(subWd));
+		localMaxima.append(E)
+
+for i in localMaxima:print(i)
 		
